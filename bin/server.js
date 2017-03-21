97a2873a9bf03f0f6af408d320f04729111d47dc
Rodrigo Ortega Cuesta <rodortcue@gmail.com>
2017-03-21 14:49:51 +0100

    Remove __CORE_HELPER__

--- bin/server.js
+++ bin/server.js
@@ -11,7 +11,6 @@ const rootDir = path.resolve(__dirname, '..');
 global.__CLIENT__ = false;
 global.__SERVER__ = true;
 global.__DISABLE_SSR__ = false;  // <----- DISABLES SERVER SIDE RENDERING FOR ERROR DEBUGGING
-global.__CORE_HELPER__ = process.env.CORE_HELPER || 'DESKTOP';
 global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
 
 // https://github.com/halt-hammerzeit/webpack-isomorphic-tools
