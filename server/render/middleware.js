b74908fff5c13eff00fc782cfe0265237b8ad1b6
Rodrigo Ortega Cuesta <rodortcue@gmail.com>
2017-03-31 15:11:48 +0200

    Working on rendering

--- server/render/middleware.js
+++ server/render/middleware.js
@@ -1,4 +1,30 @@
+import { match, createMemoryHistory as createHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+
+import createStore from 'redux/create';
+import getRoutes from 'routes';
+import pageRenderer from './pageRenderer';
+
 export default function render(req, res) {
-  /* TODO */
-  res.send('render middleware');
+  const location = req.originalUrl; // req.url ?
+  const memoryHistory = createHistory(location);
+  const store = createStore(memoryHistory);
+  const history = syncHistoryWithStore(memoryHistory, store);
+  const routes = getRoutes();
+
+  // TODO: __DISABLE_SSR__ + hydrateOnClient
+
+  match({ routes, history, location }, (error, redirectLocation, renderProps) => {
+    if (error) {
+      res.status(500).json(error);
+      // TODO: hydrateOnClient
+    } else if (redirectLocation) {
+      res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+    } else if (renderProps) {
+      const html = pageRenderer(store, renderProps, history);
+      res.status(200).send(html);
+    } else {
+      res.status(404).send('Not found');
+    }
+  });
 }
---
+++ server/render/pageRenderer.js
@@ -0,0 +1,12 @@
+export default (store, props, history) => { // TODO: props history
+  // TODO: Render things
+  console.log(store, props, history);
+
+  return `
+  <!doctype html>
+  <html>
+  <head></head>
+  <body>Hello</body>
+  </html>
+  `;
+};
